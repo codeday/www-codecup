3bc3518c873d64e1fa3c7c0ca3a0e090f5677eba
Wakeful <wakefulcloud@gmail.com>
2021-05-13 17:47:49 -0600

    Score graph WIP

--- src/pages/Scoreboard/Scoreboard.tsx
+++ src/pages/Scoreboard/Scoreboard.tsx
@@ -1,74 +1,116 @@
-import {Box, Flex, Heading} from '@chakra-ui/layout';
-import {Table, Tbody, Td, Tfoot, Th, Thead, Tr} from '@chakra-ui/table';
+import 'chartjs-adapter-luxon';
 import React from 'react';
 import styles from './Scoreboard.module.css';
+import {Box, Flex, Heading} from '@chakra-ui/layout';
+import {ChartOptions, ChartData} from 'chart.js/types/index.esm';
+import {Line} from 'react-chartjs-2';
+import {Table, Tbody, Td, Tfoot, Th, Thead, Tr} from '@chakra-ui/table';
+
+/**
+ * Options for the graph
+ */
+const graphOptions = {
+  scales: {
+    x: {
+      type: 'timeseries',
+      time: {
+        unit: 'minute'
+      }
+    }
+  }
+} as ChartOptions<'line'>;
 
 interface ScoreboardProps
 {
   teams: {
     name: string;
-    score: number;
+    scores: {
+      time: Date;
+      value: number;
+    }[];
   }[];
 }
 
-const Scoreboard: React.FC<ScoreboardProps> = (props: ScoreboardProps) => (
-  <Flex align="center" data-testid="NotFound">
-    <Box background="gray.800" padding="10px" rounded="xl" textAlign="center" width="30vw" >
-      <Heading fontSize="4xl">Scoreboard</Heading>
-      <Table variant="simple">
-        <Thead>
-          <Tr className={styles.Row}>
-            <Th>Name</Th>
-            <Th textAlign="end">Score</Th>
-          </Tr>
-        </Thead>
-        <Tbody width="100%">
-          {props.teams.map((team, index) =>
-            <Tr className={styles.Row} key={index}>
-              <Td>{team.name}</Td>
-              <Td isNumeric>{team.score}</Td>
+const Scoreboard: React.FC<ScoreboardProps> = (props: ScoreboardProps) =>
+{
+  //Generate graph data
+  const graphData = {
+    datasets: props.teams.map(team => ({
+      label: `${team.name} Score`,
+      data: team.scores.map(score =>
+      {
+        //Get the minutes
+        const minutes = score.time.getTime() / (1000 * 60);
+
+        return {
+          x: minutes,
+          y: score.value
+        };
+      })
+    }))
+  } as ChartData;
+
+  return (
+    <Flex align="center" data- testid="NotFound" >
+      <Box background="gray.800" padding="10px" rounded="xl" textAlign="center" width="30vw" >
+        <Heading fontSize="4xl">Scoreboard</Heading>
+
+        <Line data={graphData} options={graphOptions} type="line" />
+
+        <Table variant="simple">
+          <Thead>
+            <Tr className={styles.Row}>
+              <Th>Name</Th>
+              <Th textAlign="end">Score</Th>
             </Tr>
-          )}
-        </Tbody>
-        <Tfoot>
-          <Tr className={styles.Row}>
-            <Th>Name</Th>
-            <Th textAlign="end">Score</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-    </Box>
-  </Flex>
-);
+          </Thead>
+          <Tbody width="100%">
+            {props.teams.map((team, index) =>
+              <Tr className={styles.Row} key={index}>
+                <Td>{team.name}</Td>
+                <Td isNumeric>{team.scores[team.scores.length - 1].value}</Td>
+              </Tr>
+            )}
+          </Tbody>
+          <Tfoot>
+            <Tr className={styles.Row}>
+              <Th>Name</Th>
+              <Th textAlign="end">Score</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </Box>
+    </Flex >
+  );
+};
 
 //TODO: remove this; this is just an example
 Scoreboard.defaultProps = {
   teams: [
+    'Orion',
+    'Sirius',
+    'Polaris',
+    'Rigel',
+    'Antares',
+    'Vega'
+  ].map(name =>
+  {
+    //Generate a bunch of random scores
+    const scores = [];
+
+    for (let i = 0; i < 10; i++)
     {
-      name: 'Orion',
-      score: 2500
-    },
-    {
-      name: 'Sirius',
-      score: 2200
-    },
-    {
-      name: 'Polaris',
-      score: 1800
-    },
-    {
-      name: 'Rigel',
-      score: 1300
-    },
-    {
-      name: 'Antares',
-      score: 500
-    },
-    {
-      name: 'Vega',
-      score: 0
+      scores.push({
+        time: new Date(86_400 * i),
+        value: Math.round(10_000 * Math.random())
+      });
     }
-  ]
+
+    return {
+      name,
+      scores
+    };
+  })
 };
 
 export default Scoreboard;
